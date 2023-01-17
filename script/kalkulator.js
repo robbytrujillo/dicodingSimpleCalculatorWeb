3055e2056a766318f2c3bed084719b9e96b329a3
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-01-17 23:14:01 +0700

    Dicoding - Web Storage

--- script/kalkulator.js
+++ script/kalkulator.js
@@ -111,8 +111,23 @@ function performCalculation() {
             result = parseInt(calculator.firstNumber) - parseInt(calculator.displayNumber);         
         }
 
-        calculator.displayNumber = result;
+        // calculator.displayNumber = result;
+        const history = {
+            firstNumber: calculator.firstNumber,
+            secondNumber: calculator.displayNumber,
+            operator: calculator.operator,
+            result: result
+        }
+        
+    
+    
+        
+            putHistory(history);
+            calculator.displayNumber = result;
+            renderHistory();
+        
     }
 
+    
 
 
--- script/storage.js
+++ script/storage.js
@@ -72,35 +72,3 @@ function renderHistory() {
 
 renderHistory();
 
-const history = {
-    firstNumber: calculator.firstNumber,
-    secondNumber: calculator.displayNumber,
-    operator: calculator.operator,
-    result: result
-}
-
-function performCalculation() {
-    if(calculator.firstNumber == null || calculator.operator == null) {
-        alert("Anda belum menetapkan operator");
-        return;
-    } 
-
-    let result = 0;
-    if (calculator.operator === "+") {
-        result = parseInt(calculator.firstNumber) + parseInt(calculator.displayNumber);
-    } else {
-        result = parseInt(calculator.firstNumber) - parseInt(calculator.displayNumber);
-    }
-
-    //objek yang akan dikirimkan sebagai argumen fungsi put histtory
-    const history = {    
-        firstNumber: calculator.firstNumber,
-        secondNumber: calculator.displayNumber,
-        operator: calculator.operator,
-        result: result
-    }
-
-    putHistory(history);
-    calculator.displayNumber = result;
-    renderHistory();
-}
